4393e98f81a2e34e619410f84a94cfabd4d1ee2b
Spencer Pope <spenpo@spenpo.com>
2024-12-19 12:27:28 +0800

    adjust blacklist and remove block registration action

--- index.php
+++ index.php
@@ -40,18 +40,6 @@ require_once SPENPO_RESUME_PATH . 'includes/repositories/ResumeRepository.php';
 require_once SPENPO_RESUME_PATH . 'includes/api/ResumeAPI.php';
 require_once SPENPO_RESUME_PATH . 'includes/shortcodes/ResumeShortcode.php';
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
-function create_block_resume_block_init() {
-	register_block_type( __DIR__ . '/build' );
-}
-add_action( 'init', 'create_block_resume_block_init' );
-
 // Register styles
 function enqueue_resume_styles() {
     wp_enqueue_style(
